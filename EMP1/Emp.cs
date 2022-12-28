0e3656dddc050cb2ebb9687e63e8a8370d885b0b
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-28 22:41:22 +0530

    [Nehal]Adding: Calculating Wages till a condition of total working hours

--- EMP1/Emp.cs
+++ EMP1/Emp.cs
@@ -11,39 +11,45 @@ namespace EMP1
 
         public static void check()
         {
-            int EmpPerHr = 20, wage = 0;
-
-            int empHr = 0, TotalWage = 0, TotalWorkingDays = 20;
-
-
-            Random obj = new Random();
-
-            int empCheck = obj.Next(0, 3);
-
-            switch (empCheck)
+            int MaxWorkingHrs = 100;
+            int wage = 0, WagePerHr = 20, NoOfWorkingDays = 20;
+            int totalEmpHr = 0, TotalWorkingDays = 0, empHr = 0;
+            while (totalEmpHr <= MaxWorkingHrs && TotalWorkingDays <= NoOfWorkingDays)
             {
+                TotalWorkingDays++;
 
-                case PartTime:
+                Random obj = new Random();
 
-                    empHr = 4 * TotalWorkingDays;
-                    break;
+                int empCheck = obj.Next(0, 3);
 
-                case FullTime:
+                switch (empCheck)
+                {
 
-                    empHr = 8 * TotalWorkingDays;
-                    break;
+                    case PartTime:
 
-                default:
-                    empHr = 0;
+                        empHr = 4 * TotalWorkingDays;
+                        break;
 
-                    break;
+                    case FullTime:
 
+                        empHr = 8 * TotalWorkingDays;
+                        break;
+
+                    default:
+                        empHr = 0;
+
+                        break;
+
+                }
+
+
+                totalEmpHr = empHr;
+
+                Console.WriteLine("Days: " + TotalWorkingDays + " WorkingHr " + empHr);
             }
+               wage = wage+ empHr* WagePerHr;
 
-            wage = empHr * EmpPerHr;
-            TotalWage= wage;
-
-            Console.WriteLine("The total wage is :" + TotalWage);
+             Console.WriteLine("The total wage"+wage);
 
         }
     
