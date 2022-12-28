e2faa31ede0ced7c4b57050c6217a73479698fa5
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-28 21:40:04 +0530

    [Nehal]Adding: Employee is present or absent

---
+++ EMP1/Emp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EMP1
+{
+    public class Emp
+    {
+        public static void check()
+        {
+            int FullTime = 1;
+
+            Random obj = new Random();
+
+            int empCheck = obj.Next(0, 2);
+
+            if (empCheck == FullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+
+            else
+            {
+                Console.WriteLine("Employee is absent");
+
+            }
+        }
+    }
+}
